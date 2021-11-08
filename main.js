ca235152cc0b5beafd070215b95d057049d32623
ejoseano <ejoseano@gmail.com>
2021-11-08 15:58:56 -0400

    penutima-version

--- main.js
+++ main.js
@@ -17,10 +17,12 @@ for (const link of links) {
   })
 }
 // mudar o header da página quando dar o scroll //
-const header = document.querySelector('#header')
-const navHeight = header.offsetHeight
 
-window.addEventListener('scroll', function () {
+
+function changeHeaderWhenScroll() {
+  const header = document.querySelector('#header')
+  const navHeight = header.offsetHeight
+
   if (window.scrollY >= navHeight) {
     // scroll é maior que a altura do header
     header.classList.add('scroll')
@@ -28,7 +30,8 @@ window.addEventListener('scroll', function () {
     //menor que a altura do header
     header.classList.remove('scroll')
   }
-})
+}
+
 
 /* Testimonials caroussel slider swiper */
 const swiper = new Swiper('.swiper', {
@@ -55,6 +58,27 @@ scrollReveal.reveal(
 #services header, #services .card,
 #testimonials header, #testimonials .testimonials
 #contact .text, #contact .links
+footer .brand, footer .social
 `,
   { interval: 100 }
 )
+
+/* Botão volta para o topo */
+
+
+function backToTop() {
+  const backToTopButton = document.querySelector('.back-to-top')
+
+  if(window.scrollY >= 560) {
+    backToTopButton.classList.add('show')
+    } else {
+      backToTopButton.classList.remove('show')
+    }
+}
+
+
+/*WhenScroll*/
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScroll()
+  backToTop()
+  })
