dc20a2552914018bfd1753169bdb889ebfc01342
ejoseano <ejoseano@gmail.com>
2021-11-09 23:04:40 -0400

    Final-Version-NLW

--- main.js
+++ main.js
@@ -18,11 +18,10 @@ for (const link of links) {
 }
 // mudar o header da página quando dar o scroll //
 
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
 
 function changeHeaderWhenScroll() {
-  const header = document.querySelector('#header')
-  const navHeight = header.offsetHeight
-
   if (window.scrollY >= navHeight) {
     // scroll é maior que a altura do header
     header.classList.add('scroll')
@@ -32,7 +31,6 @@ function changeHeaderWhenScroll() {
   }
 }
 
-
 /* Testimonials caroussel slider swiper */
 const swiper = new Swiper('.swiper', {
   slidesPerView: 1,
@@ -40,7 +38,13 @@ const swiper = new Swiper('.swiper', {
     el: '.swiper-pagination'
   },
   mousewheel: true,
-  keyboard: true
+  keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true
+    }
+  }
 })
 
 /*ScrollReveal: Mostra elementos enquanto da scroll na página*/
@@ -65,20 +69,43 @@ footer .brand, footer .social
 
 /* Botão volta para o topo */
 
+const backToTopButton = document.querySelector('.back-to-top')
 
 function backToTop() {
-  const backToTopButton = document.querySelector('.back-to-top')
-
-  if(window.scrollY >= 560) {
+  if (window.scrollY >= 560) {
     backToTopButton.classList.add('show')
-    } else {
-      backToTopButton.classList.remove('show')
-    }
+  } else {
+    backToTopButton.classList.remove('show')
+  }
 }
 
+/* Menu ativo conforme a seção */
+const sections = document.querySelectorAll('main section[id]')
+function activeteMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
 
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active')
+    } else {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.remove('active')
+    }
+  }
+}
 /*WhenScroll*/
 window.addEventListener('scroll', function () {
   changeHeaderWhenScroll()
   backToTop()
-  })
+  activeteMenuAtCurrentSection()
+})
